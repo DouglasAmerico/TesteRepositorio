9eaaa963ffc0b87e7351c9a3f179640310f6b6e5
unknown <Administrador@gi02tads30.integrado.int>
2024-05-20 22:09:25 -0300

    Começando o arquivo

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-console.log("Hello Word");
-console.log("Funcionou");
+let leitor = require("prompt-sync")();
+
+let aluno = {
+    nome : null,
+    idade: null,
+    sexo: null,
+    notas : [],
+    media: 0
+}
+
+let turma;
+let qtdDeNotas= leitor("Informa quantas notas teremos por aluno: ");
+
+for (let index = 0; index < 1; index++) {
+    aluno = {aluno}
+    aluno.nome= leitor("Informe o nome do "+Number(index+1)+" ° aluno: ");
+    aluno.idade= leitor("Informe a idade do(a) aluno(a)"+aluno.nome+": ");
+    aluno.sexo= leitor("Informe o sexo do(a) aluno(a)"+aluno.nome+": ");
+    for (let i = 0; i < qtdDeNotas; i++) {
+        aluno.notas[i]= leitor("Informe a nota "+Number(i+1)+" do(a) aluno(a)"+aluno.nome+": ");
+        aluno.media= aluno.notas[i] + aluno.media;
+    }
+    turma = aluno;
+}
+
+console.log(turma);
